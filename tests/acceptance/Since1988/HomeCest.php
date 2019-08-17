3c26a1e5e37f37a325bd13154c23b83a4a08ec37
neamil <johannes.seipelt@cypelt.de>
2019-08-17 22:03:38 +0200

    geben Sie eine Commit-Beschreibung für Ihre Änderungen ein. Zeilen, verride file - we dont need them -fix namespace stuff for the HomeCest -fix the acceptance yml, issue here was the "enabled" vs "config" section -changed selenium image version because the latest version chrome driver has a bug

--- tests/acceptance/Since1988/HomeCest.php
+++ tests/acceptance/Since1988/HomeCest.php
@@ -1,13 +1,10 @@
-<?php namespace Since1988;
-
-
-use AcceptanceTester;
+<?php
 
 class HomeCest
 {
     public function _before(AcceptanceTester $I)
     {
-        $I->amOnPage('https://since1988.de.ddev.site/index.php?id=28');
+        $I->amOnPage('/index.php?id=28');
     }
 
     // tests
